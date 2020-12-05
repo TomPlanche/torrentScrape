3433e774e4c9b5c1a5021eb64e664fb27b6ef174
Tom Planche <58936594+TomPlanche@users.noreply.github.com>
2020-12-05 12:53:20 +0100

    Update torrentScrapeSansMail.py

--- torrentScrapeSansMail.py
+++ torrentScrapeSansMail.py
@@ -17,16 +17,18 @@ driver.get(site)
 
 soupe = BeautifulSoup(driver.page_source, 'html.parser')
 
+# articles links
 liens_articles = [h2.find('a')['href'] for h2 in soupe.find_all('h2', class_ = 'post-title')]
 
 for article in liens_articles:
     driver.get(article)
     soupe_article = BeautifulSoup(driver.page_source, 'html.parser')
-
+    # title
     titre_article = driver.find_element_by_xpath('//*[@id="main"]/article/header/h1').text
     date_article = driver.find_element_by_xpath('//*[@id="main"]/article/header/p/time').text
     categorie_article = [elem.text for elem in soupe_article.find('p', class_ = 'meta post-info').find_all('a')]
     image_article = soupe_article.find('div', class_ = 'feature-img').find('img')['src']
+    # size of the article
     taille_article = soupe_article.find('table', class_ = 'torrentInfoTable').find_all('td')[1].text
     description_article = soupe_article.find('section', class_ = 'post_content clearfix').find('p').text
 
@@ -36,4 +38,3 @@ for article in liens_articles:
           f"Taille : {taille_article}\n"
           f"lien : {article}")
     print()
-
